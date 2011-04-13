fc3c8e3ac9cbc7e57fc1a43a7e909fdfb14129e4
Daniel Bachhuber <danielbachhuber@gmail.com>
2011-04-13 18:17:42 +0000

    Formatting changes

--- home.php
+++ home.php
@@ -6,20 +6,22 @@
 <ul class="thumbs">
 	<?php while ($home_query->have_posts()) : $home_query->the_post();
 	    $do_not_duplicate = $post->ID; $i++; ?>
-	    <li class="post-<?php the_ID(); ?> thumb-big"><?php get_the_image( array( 'custom_key' => array( 'thumbnail' ), 'default_size' => 'thumbnail', 'width' => '160', 'height' => '160' ) ); ?></li>
+	    <li class="post-<?php the_ID(); ?> thumb-big">
+		<?php get_the_image( array( 'custom_key' => array( 'thumbnail' ), 'default_size' => 'thumbnail', 'width' => '160', 'height' => '160' ) ); ?>
+		</li>
     <?php endwhile; wp_reset_query(); $i = 0; ?>
 </ul>
 </div>
 
-    <div id="branding">
-        <div class="brand-wrap">
-        <h2><a href="<?php bloginfo('home'); ?>/" title="Home"><?php bloginfo('name'); ?></a></h2>
-        <div class="description"><?php bloginfo('description'); ?></div> 
-        <?php fullscreen_theme_nav(); ?>
-        <div class="go-left"><a href="#"><img src="<?php bloginfo("template_directory"); ?>/images/back.gif" class="go-left" alt="back" /></a></div>
-    	<div class="go-right"><a href="#"><img src="<?php bloginfo("template_directory"); ?>/images/forward.gif" class="go-right" alt="forward" /></a></div>
-	    </div>
-	</div>
+<div id="branding">
+	<div class="brand-wrap">
+	<h2><a href="<?php bloginfo('home'); ?>/" title="Home"><?php bloginfo('name'); ?></a></h2>
+	<div class="description"><?php bloginfo('description'); ?></div> 
+	<?php fullscreen_theme_nav(); ?>
+	<div class="go-left"><a href="#"><img src="<?php bloginfo("template_directory"); ?>/images/back.gif" class="go-left" alt="back" /></a></div>
+<div class="go-right"><a href="#"><img src="<?php bloginfo("template_directory"); ?>/images/forward.gif" class="go-right" alt="forward" /></a></div>
+</div>
+</div>
 
 <!-- Begin middle thumbnails -->
 <div class="home-thumbs middle-thumbs">
